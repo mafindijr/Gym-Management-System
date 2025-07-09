5413eb89919514cb34118aea39bf681089e91c81
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-09 10:27:39 -0700

    still working on the header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className='flex items-center justify-between bg-homebg'>
+    <>
+    <header className='flex items-center justify-between pr-20 pl-20'>
         <div className=''>
             <h1>Fitness Hub</h1>
         </div>
         <div>
-            <div className='flex s'>
+            <div className='flex gap-2'>
                 <a href="#">Home</a>
                 <a href="#">About</a>
                 <a href="#">Services</a>
                 <a href="#">Contact</a>
             </div>
         </div>
-        <div className=''>
+        <div className='bg-btnprimary w-[80px] text-center font-poppins text-text h-[40px] p-2 rounded-[20px]'>
             <button>Sign Up</button>
         </div>
     </header>
+    </>
   )
 }
