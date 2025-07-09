ec318eff250c46e663d2a1abf50dc83a17291aec
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-09 07:40:55 -0700

    feat: started working on the header section

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,1 +1,22 @@
-rfc
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='flex items-center justify-between bg-homebg'>
+        <div className=''>
+            <h1>Fitness Hub</h1>
+        </div>
+        <div>
+            <div className='flex s'>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+        <div className=''>
+            <button>Sign Up</button>
+        </div>
+    </header>
+  )
+}
