b403ff7d9efd4413e027217670eca967c181e695
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-11 23:48:12 -0700

    feat: created the footer children

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 
 export default function Footer() {
 
-    
-  return (
-    <div>
 
-    </div>
+  return (
+    <>
+        <div className='flex items-top justify-between'>
+            <div><a href="#">Privacy Policy</a></div>
+            <div className='flex inline-block justify-center'>
+                <div><a href="#">Terms of services</a></div>
+                <div></div>
+                <div><a href="#">&copy; 2025 FitnessHub. All rights reserved</a></div>
+            </div>
+            <div><a href="#">Contact Us</a></div>
+        </div>
+    </>
   )
 }
