424ae1fc84cf4ba394ba9f779cf48f61a2cfa621
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-13 00:43:27 -0700

    feat: added social handles to the footer

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import FacebookIcon from "../assets/Svgs/facebook.svg";
+import TwitterIcon from "../assets/Svgs/twitter.svg";
 
 export default function Footer() {
 
+    const svgIcons = [
+        { icon: FacebookIcon, label: 'go to our facebook page' },
+        { icon: TwitterIcon, label: 'go to out twitter page' }
+    ]
 
   return (
     <>
@@ -9,7 +14,22 @@ export default function Footer() {
             <div><a href="#">Privacy Policy</a></div>
             <div className='flex inline-block justify-center'>
                 <div><a href="#">Terms of services</a></div>
-                <div></div>
+                <div>
+                   {svgIcons.map((handle, index) => (
+                        <a
+                            href="#"
+                            key={index}
+                           
+                        >
+                            <img
+                                src={handle.icon}
+                                alt={handle.label}
+                                className="w-8 h-8"
+                            />
+                        
+                        </a>
+                    ))}
+                </div>
                 <div><a href="#">&copy; 2025 FitnessHub. All rights reserved</a></div>
             </div>
             <div><a href="#">Contact Us</a></div>
