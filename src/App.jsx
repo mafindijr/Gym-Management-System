f909da8a67fc88a2663ee55b15336f56051e348d
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-17 23:51:16 -0700

    import the new component for the membes for reviewin the output

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Components/header";
 import LandingPage from './Components/landingPage';
 import Footer from "./Components/footer";
-import Admin from "./Components/admin-dashboard";
+import MembersDashboard from "./Components/members";
 
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
         <Header />
         <LandingPage />
         <Footer />
-        <Admin />
+        <MembersDashboard />
     </>
   )
 }
