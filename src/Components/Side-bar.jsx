866d72ce7aa5ad39904dcabee4d1e9aa56975f12
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-23 06:38:59 -0700

    added style for consistency

--- src/Components/Side-bar.jsx
+++ src/Components/Side-bar.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function SideBar() {
 
     const navs = [
-            { to: "/admin", name: "Dashboard", icon: "X"},
-            { to: "member", name: "Members", icon: "X"},
-            { name: "Classes", icon: "X"},
-            { name: "Trainers", icon: "X"},
-            { name: "Billing", icon: "X"},
-            { name: "Settings", icon: "X"}
+            { to: "/admin",   name: "Dashboard", icon: "X"},
+            { to: "/member",   name: "Members",   icon: "X"},
+            { to: "/classes",  name: "Classes",   icon: "X"},
+            { to: "/trainers", name: "Trainers",  icon: "X"},
+            { to: "/billling", name: "Billing",   icon: "X"},
+            { to: "/settings", name: "Settings",  icon: "X"}
         ];
 
 
@@ -20,11 +20,11 @@ export default function SideBar() {
                     <p className='text-[14px] leading-6 text-adminsmtext'>Admin</p>
                 </div>
                 <div>
-                    <ul className='flex flex-col gap-4'>
+                    <ul className='flex flex-col gap-4 items-center text-center justify-center'>
                       {
                         navs.map((nav, index ) => {
                             return (
-                                <NavLink to={nav.to} className='flex gap-2 w-full leading-5.4 text-[14px]  font-montserrat bg-[#223649] p-2 rounded-full cursor-pointer' key={index}>
+                                <NavLink to={nav.to} className={({ isActive }) => `flex gap-2 w-full leading-5.4 text-[14px]  font-montserrat p-2 rounded-full cursor-pointer transition hover:bg-[#223649] ${isActive? "bg-[#223649]" : "" }`} key={index}>
                                     <span>{ nav.icon }</span>
                                     <a href="#">{ nav.name }</a>
                                 </NavLink>
