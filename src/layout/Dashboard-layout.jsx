415f751548454cce2cb9db19af9794186a4b21aa
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-23 06:17:44 -0700

    import dide bar component to the layout

--- src/layout/Dashboard-layout.jsx
+++ src/layout/Dashboard-layout.jsx
@@ -1,7 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from '../Components/Side-bar';
 
 export default function DashboardLayout() {
   return (
-    <div>AdminDashboardLayout</div>
+    <div className="flex min-h-screen">
+
+      <Sidebar />
+
+      <main className="flex-1 p-4">
+        <Outlet />
+      </main>
+
+    </div>
   )
 }
