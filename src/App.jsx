f630d764e8ea3a3cdcc765871b37fb33b8e2139d
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-23 06:16:46 -0700

    created a routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import DashboardLayout from "./layout/Dashboard-layout";
+import Admin from "./pages/admin-dashboard";
+import MembersDashboard from "./pages/members";
 import Home from "./views/Home"
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-        <Home />
+    <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/" element={<DashboardLayout />}>
+          <Route  path="/admin" element={<Admin />} />
+          <Route path="/member" element={<MembersDashboard />} />
+        </Route>
+
+    </Routes>
     </>
   )
 }
