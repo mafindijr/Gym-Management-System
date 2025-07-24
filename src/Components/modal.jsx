f15f2444bca301ee1255d99214caf1882d2e9718
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-24 04:28:23 -0700

    created a modal dialog for the member dashboard

---
+++ src/Components/modal.jsx
@@ -0,0 +1,23 @@
+import React, { Children } from 'react'
+import { X } from 'lucide-react';
+
+export default function Modal({ children }) {
+
+        return (
+            <div>
+
+                <div className='fixed inset-0 bg-black/80 flex items-center justify-center z-50'>
+                    <div className="bg-[#223649] p-6 rounded-[8px] w-[50%] shadow-lg">
+                        <button
+                        // onClick={onClose}
+                        className='flex justify-end cursor-pointer w-full'
+                        >
+                        <X color='black' size={25}/>
+                        </button>
+                        {children}
+                    </div>
+                </div>
+                
+            </div>
+       )
+}
