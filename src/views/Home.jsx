f2d2ce95a1b03f028e753d1bc112972bdba73379
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-25 04:04:54 -0700

    remove unneccessary import from the home component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,11 @@
 import LandingPage from '../Components/landingPage';
 import Layout from '../layout/layout';
-import DashboardLayout from '../layout/Dashboard-layout';
 
 export default function Home() {
   return (
     <Layout>
 
         <LandingPage />
-        <DashboardLayout />
     </Layout>
   )
 }
