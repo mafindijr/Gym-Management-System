2a43b247351a78957a6549cf3dd48054fe5b61d9
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-07-26 02:58:22 -0700

    working on form render

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -66,16 +66,16 @@ export default function MembersDashboard() {
                                     <div><span>Action</span></div>
                                 </div>
                                 <div id="tableBody" className="">
-                                <div className='flex justify-around items-center text-[#8fadcc] p-4 border-[#e5e8eb] border-b-1'>
-                                    
+                                  {data.map((item, index) => ( <div key={index} className='flex justify-around items-center text-[#8fadcc] p-4 border-[#e5e8eb] border-b-1'>
+                                
                                     <div className="w-[185px] text-center text-[#e5e8eb]">
-                                        <span>Abdulrazak mafindi</span>
+                                        <span>{item.name}</span>
                                     </div>
                                     <div className="w-[185px] text-center">
-                                        <span>Premium</span>
+                                        <span>{item.membership}</span>
                                     </div>
                                     <div className="w-[185px] text-center">
-                                        <span className="bg-[#223649] px-8 py-2 font-semibold rounded-md cursor-pointer text-[#e5e8eb]">Active</span>
+                                        <span className="bg-[#223649] px-8 py-2 font-semibold rounded-md cursor-pointer text-[#e5e8eb]">{item.status}</span>
                                     </div>
                                     <div className="w-[185px] text-center">
                                         <span>2 days ago</span>
@@ -83,35 +83,8 @@ export default function MembersDashboard() {
                                     <div className="w-[185px] text-center">
                                         <span className="font-semibold cursor-pointer">View</span>
                                     </div>
-                                </div>
-                                <div className='flex justify-around items-center p-4 border-b-1'>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                </div>
-                                <div className='flex justify-around items-center p-4 border-b-1'>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                </div>
-                                <div className='flex justify-around items-center p-4 border-b-1'>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                </div>
-                                <div className='flex justify-around items-center p-4 border-b-1'>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                    <div>rows</div>
-                                </div>
+                                </div>))}
+                                
                             </div>
                         </div>
                         
