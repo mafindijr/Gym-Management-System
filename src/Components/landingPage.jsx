7fe2dfa2b49463dfce45285548b7098d98a9ed94
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-08-12 08:20:16 -0700

    remove some whitespace in the code

--- src/Components/landingPage.jsx
+++ src/Components/landingPage.jsx
@@ -38,10 +38,8 @@ export default function LandingPage() {
               <div className="flex flex-1 gap-3 max-w-[480px] flex-col items-stretch px-4 py-3">
                 <button
                   className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#223649] text-white text-sm font-Poppins font-bold leading-normal tracking-[0.015em] w-full"
-                >
-                  
-                    <span className="truncate">Student Login</span>
-                  
+                >           
+                    <span className="truncate">Student Login</span> 
                 </button>
                 <button
                   className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#223649] text-white text-sm font-Poppins font-bold leading-normal tracking-[0.015em] w-full"
