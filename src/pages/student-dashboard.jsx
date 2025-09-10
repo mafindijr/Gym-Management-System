666fb0b10842a62f79b5f0c2aac4c43fc8ed768d
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-10 05:07:16 -0700

    feat: create StudentDashboard component with welcome message

---
+++ src/pages/student-dashboard.jsx
@@ -0,0 +1,14 @@
+export default function StudentDashboard() {
+  return (
+    <div>
+      <main className='flex flex-col col-span-4 justify-center gap-8 w-full'>
+        <div>
+          <h1 className='text-[36px] leading-[40px] font-bold font-montserrat'>Student Dashboard</h1>
+        </div>
+        <div>
+          <p className='text-[18px]'>Welcome, Student! Here you can view your classes, attendance, and more.</p>
+        </div>
+      </main>
+    </div>
+  );
+}
