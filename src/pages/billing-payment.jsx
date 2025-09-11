ef55301096e626a00dcff862c6dd5d7cac07ba59
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-11 12:05:12 -0700

    remove and empty class

--- src/pages/billing-payment.jsx
+++ src/pages/billing-payment.jsx
@@ -21,14 +21,14 @@ export default function BillingPayment() {
 
   return (
     <>
-         <main className='flex flex-col col-span-4 gap-4 w-full'>
+         <main className='flex flex-col col-span-4 gap-4'>
                     <div className='flex justify-between items-center'>
                         <div className='flex flex-col gap-4'>
                         <h1 className='text-[32px] leading-[40px] font-bold font-montserrat'>Billing & Payments</h1>
                         </div>
                     </div>
         
-                    <div className='flex flex-col gap-4 text-[14px] leading-[21px]'>
+                    <div className='w-[902px] flex flex-col gap-4 text-[14px] leading-[21px]'>
                         <div className='inline-flex gap-4 border-b-1 border-[#334d66]'>
                             <span className='border-b-2 pb-2'>Overview</span>
                             <span className='border-b-2 pb-2'>Invoices</span>
@@ -38,24 +38,28 @@ export default function BillingPayment() {
                             <div>
                                 <h4>Summary</h4>
                             </div>
-                            <div>
+                            <div className='w-[908px]'>
                                 <div className='flex gap-4'>
                                     <div className='w-[280px] h-[120] rounded-[12px] p-4 text-left bg-[#223649]'>
-                                        <p className='font-semibold text-[16px] leading-[24px]'>Total Members</p>
-                                        <p className='font-bold text-[24px] leading-[30px]'>250</p>
+                                        <p className='font-semibold text-[16px] leading-[24px]'>Total Revenue</p>
+                                        <p className='font-bold text-[24px] leading-[30px]'>#15,500,000</p>
                                     </div>
                                     <div className='w-[280px] h-[120] rounded-[12px] p-4 text-left bg-[#223649]'>
-                                        <p className='font-semibold text-[16px] leading-[24px]'>Active Classes</p>
-                                        <p className='font-bold text-[24px] leading-[30px]'>15</p>
+                                        <p className='font-semibold text-[16px] leading-[24px]'>Outstanding Balance</p>
+                                        <p className='font-bold text-[24px] leading-[30px]'>#2,300,00</p>
                                     </div>
                                     <div className='w-[280px] h-[120] rounded-[12px] p-4 text-left bg-[#223649]'>
-                                        <p className='font-semibold text-[16px] leading-[24px]'>Trainers</p>
-                                        <p className='font-bold text-[24px] leading-[30px]'>8</p>
+                                        <p className='font-semibold text-[16px] leading-[24px]'>Average Payment</p>
+                                        <p className='font-bold text-[24px] leading-[30px]'>#200,000</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="w-[928px] h-[480px] flex flex-col border-1 inset-1 border-[#334d66] rounded-[12px]">
+                        <div className='flex flex-col gap-4'>
+                            <div>
+                                <h2>Recent Payments</h2>
+                            </div>
+                        <div className="w-[908px] h-[480px] flex flex-col border-1 inset-1 border-[#334d66] rounded-[12px]">
                                 <div className='flex justify-around items-center bg-[#172633] p-4 border-[#e5e8eb] border-b-1'>
                                     <div><span>Member</span></div>
                                     <div><span>Amount</span></div>
@@ -80,6 +84,7 @@ export default function BillingPayment() {
                                         </div>
                                     ))}
                                 </div>
+                            </div>
                         </div>
                         
                     </div>
