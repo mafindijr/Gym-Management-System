0fd455d9db6055de7b4b49b031694e94bcec06af
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-13 01:58:31 -0700

    fix: student settings

--- src/pages/student-settings.jsx
+++ src/pages/student-settings.jsx
@@ -4,7 +4,9 @@ export default function StudentSettings() {
   return (
     <main className='flex flex-col col-span-4 gap-4 w-full'>
       <h1 className='text-[32px] leading-[40px] font-bold font-montserrat'>Settings</h1>
+      <div>
       <StudentSettingsForm />
+      </div>
     </main>
   );
 }
