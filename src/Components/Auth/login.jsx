ebd90ccdf13f9c7fe6fd306fa685634f7880eb14
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-15 08:22:39 -0700

    feat: created the login page structure

--- src/Components/Auth/login.jsx
+++ src/Components/Auth/login.jsx
@@ -4,7 +4,23 @@ export default function LoginPage() {
 
     return (
         <>
-        
+            <div className=" items-center bg-[#223649] p-6 rounded-lg shadow flex flex-col gap-4">
+                <div>
+                    <h2>Welcome Back</h2>
+                </div>
+                <div>
+                    <input type="text" />
+                </div>
+                <div>
+                    <input type="text" />
+                    <span>Forgot Password</span>
+                </div>
+                <div>
+                    <button>Log In</button>
+                    <span>Don't have an account? Sign Up</span>
+                </div>
+                
+            </div>
         </>
     );
 }
