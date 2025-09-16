6cad3b8c2797156352b5aa24acdddb2e22ce5a1b
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-16 07:57:36 -0700

    create and style some inputs

--- src/Components/Auth/login.jsx
+++ src/Components/Auth/login.jsx
@@ -4,15 +4,28 @@ export default function LoginPage() {
 
     return (
         <>
+        <form>
             <div className=" items-center bg-[#223649] p-6 rounded-lg shadow flex flex-col gap-4">
                 <div>
                     <h2>Welcome Back</h2>
                 </div>
                 <div>
-                    <input type="text" />
+                    <input 
+                    type="email"
+                    name='email'
+                    className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px]"
+                    placeholder='Enter Email'
+                    required
+                    />
                 </div>
                 <div>
-                    <input type="text" />
+                    <input 
+                    type="password"
+                    name='email'
+                    className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px]"
+                    placeholder='Enter Email'
+                    required
+                    />
                     <span>Forgot Password</span>
                 </div>
                 <div>
@@ -21,6 +34,7 @@ export default function LoginPage() {
                 </div>
                 
             </div>
+         </form>    
         </>
     );
 }
