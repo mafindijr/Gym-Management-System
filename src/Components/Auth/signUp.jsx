123f18328013c90bdfa2d637110c05cadd63bd18
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-16 08:36:22 -0700

    finish sigup page

--- src/Components/Auth/signUp.jsx
+++ src/Components/Auth/signUp.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function signUpPage() {
+export default function SignUpPage() {
   return (
     <form>
             <div className="w-full bg-[#223649] p-6 rounded-lg shadow flex flex-col gap-4">
                 <div>
-                    <h2 className='text-[28px] leading-[35px] font-bold text-center'>Welcome Back</h2>
+                    <h2 className='text-[28px] leading-[35px] font-bold text-center'>Welcome! Sign Up</h2>
                 </div>
                 <div>
                     <input 
@@ -13,6 +13,15 @@ export default function signUpPage() {
                     name='fullName'
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
                     placeholder='Full Name'
+                    required
+                    />
+                </div>
+                <div>
+                    <input 
+                    type="Email"
+                    name='Email'
+                    className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
+                    placeholder='Enter Email'
                     required
                     />
                 </div>
@@ -35,7 +44,7 @@ export default function signUpPage() {
                     />
                 </div>
                 <div className='flex flex-col items-center'>
-                    <button className='bg-btnprimary w-full pl-4 pr-4 rounded-[24px] text-[14px] font-bold font-poppins text-center cursor-pointer h-[40px]'>Log In</button>
+                    <button className='bg-btnprimary w-full pl-4 pr-4 rounded-[24px] text-[14px] font-bold font-poppins text-center cursor-pointer h-[40px]'>Sign Up</button>
                     <span className='text-[14px] leading-[21px] text-[#91adc9]'>Already have an account? Log In</span>
                 </div>
                 
