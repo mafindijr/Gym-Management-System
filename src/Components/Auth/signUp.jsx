6002b7e54fc1c10c495b8e3690452786064a08ea
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-16 08:29:20 -0700

    style: styling the form

--- src/Components/Auth/signUp.jsx
+++ src/Components/Auth/signUp.jsx
@@ -10,21 +10,29 @@ export default function signUpPage() {
                 <div>
                     <input 
                     type="text"
-                    name='email'
+                    name='fullName'
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
-                    placeholder='Enter Email'
+                    placeholder='Full Name'
                     required
                     />
                 </div>
                 <div>
                     <input 
                     type="password"
-                    name='email'
+                    name='createPassword'
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
-                    placeholder='Enter Email'
+                    placeholder='Create Password'
                     required
                     />
-                    <span className='text-[14px] leading-[21px] text-[#91adc9]'>Forgot Password</span>
+                </div>
+                <div>
+                    <input 
+                    type="password"
+                    name='confirmEmail'
+                    className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
+                    placeholder='Confirm Password'
+                    required
+                    />
                 </div>
                 <div className='flex flex-col items-center'>
                     <button className='bg-btnprimary w-full pl-4 pr-4 rounded-[24px] text-[14px] font-bold font-poppins text-center cursor-pointer h-[40px]'>Log In</button>
