b0f72b3775e16e0657b12135660b6284265c8937
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-17 08:56:08 -0700

    fix: tailwind css double text style on a single line

--- src/Components/Auth/reset-passord/resetPassword.jsx
+++ src/Components/Auth/reset-passord/resetPassword.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ResetPassword() {
   return (
     <div>
-      <h2 className='font-montserrat font-[700] font-bold text-[32px] leading-[44px] text-center hover:scale-105 transition-transform duration-300'>Set New Password</h2>
+      <h2 className='font-montserrat font-bold text-[32px] leading-[44px] text-center hover:scale-105 transition-transform duration-300'>Set New Password</h2>
         <p className='font-montserrat font-[400] text-[18px] leading-[28px] text-center mt-2'>Create a new password for your account</p>
 
         <form>
