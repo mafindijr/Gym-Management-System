603f7b1cdcc5c2fc8a0c61b146026a6157869c90
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-18 15:17:58 -0700

    import usedate hook

--- src/Components/Auth/reset-passord/resetPassword.jsx
+++ src/Components/Auth/reset-passord/resetPassword.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function ResetPassword() {
+
+      const [openResetPassword, setOpenResetPassword] = useState(false);
+      const [isSubmitting, setIsSubmitting] = useState(false);
+
+
   return (
         <div>
             <h4 className='font-montserrat font-[700] font-bold text-[20px] leading-[30px] text-center mb-2 mt-2'>Reset Your Password</h4>
