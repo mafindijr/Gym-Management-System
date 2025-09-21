d186242ea5b471121e374c51a5b7f41822c0a343
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-21 14:34:48 -0700

    feat: make payments detail a read only

--- src/Components/student-settings-form.jsx
+++ src/Components/student-settings-form.jsx
@@ -88,50 +88,50 @@ export default function StudentSettingsForm() {
              <label className="flex flex-col gap-1 w-full">
                 Cardholder Name
                 <input
-                className="p-2 rounded bg-[#172633] border border-[#334d66] text-white"
+                className="p-2 rounded outline-0 bg-[#172633] border border-[#334d66] text-white"
                 type="text"
                 name="cardName"
                 value={form.cardName}
                 onChange={handleChange}
                 placeholder="Name on Card"
-                required
+                readOnly
                 />
             </label>
             <label className="flex flex-col gap-1 w-full">
                 Card Number
                 <input
-                className="p-2 rounded bg-[#172633] border border-[#334d66] text-white"
+                className="p-2 rounded outline-0 bg-[#172633] border border-[#334d66] text-white"
                 type="text"
                 name="cardNumber"
                 value={form.cardNumber}
                 onChange={handleChange}
                 placeholder="Card Number"
-                required
+                readOnly
                 />
             </label>
             <div className="grid grid-cols-2 gap-4 w-full items-center">
                 <label className="flex flex-col gap-1 flex-1">
                 Expiry Date
                 <input
-                    className="p-2 rounded bg-[#172633] border border-[#334d66] text-white"
+                    className="p-2 rounded outline-0 bg-[#172633] border border-[#334d66] text-white"
                     type="text"
                     name="expiry"
                     value={form.expiry}
                     onChange={handleChange}
                     placeholder="MM/YY"
-                    required
+                    readOnly
                 />
                 </label>
                 <label className="flex flex-col gap-1 flex-1">
                 CVV
                 <input
-                    className="p-2 rounded bg-[#172633] border border-[#334d66] text-white"
+                    className="p-2 rounded outline-0 bg-[#172633] border border-[#334d66] text-white"
                     type="password"
                     name="cvv"
                     value={form.cvv}
                     onChange={handleChange}
                     placeholder="CVV"
-                    required
+                    readOnly
                 />
                 </label>
           </div>
