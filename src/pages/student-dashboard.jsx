6ffd6227d3c8771853d7ac3efec7d5c398681db7
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-24 15:04:55 -0700

    uploaded images for the class

--- src/pages/student-dashboard.jsx
+++ src/pages/student-dashboard.jsx
@@ -12,11 +12,17 @@ export default function StudentDashboard() {
           <h4>Enroll Classes</h4>
           <div className="inline-flex gap-8 p-8">
             <div>
-              <img src="" alt="assets/images/yogaimage.png" loading="lazy" />
+              <img src="/src/assets/images/yogaimage.png" alt="yoga image" loading="lazy" />
               <span>Yoga Class</span>
             </div>
-            <div></div>
-            <div></div>
+            <div>
+              <img src="/src/assets/images/cardio.png" alt="cardio image" loading="lazy" />
+              <span>Cardio Class</span>
+            </div>
+            <div>
+              <img src="\src\assets\images\strenth.png" alt="Stremthing image" loading="lazy" />
+              <span>Strnthing Class</span>
+            </div>
           </div>
         </div>
       </main>
