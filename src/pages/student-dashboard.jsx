4514bb81571c77ddcb5320923a436db6a23ae82e
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-24 14:51:46 -0700

    working on student dashboard

--- src/pages/student-dashboard.jsx
+++ src/pages/student-dashboard.jsx
@@ -11,7 +11,10 @@ export default function StudentDashboard() {
         <div>
           <h4>Enroll Classes</h4>
           <div className="inline-flex gap-8 p-8">
-            <div></div>
+            <div>
+              <img src="" alt="assets/images/yogaimage.png" loading="lazy" />
+              <span>Yoga Class</span>
+            </div>
             <div></div>
             <div></div>
           </div>
