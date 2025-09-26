a0b6cbe36d92b71dd6bcfd47a1a351e5298e9d88
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-26 15:35:18 -0700

    feat: working and improving the Ui

--- src/pages/student-dashboard.jsx
+++ src/pages/student-dashboard.jsx
@@ -3,29 +3,80 @@
 
 export default function StudentDashboard() {
   return (
-    <div>
-      <main className='w-full'>
+    <div className="w-full">
+
+      <main className='flex flex-col border-2 border-amber-600'>
         <div>
-          <h1 className='text-[36px] leading-[40px] font-bold font-montserrat'>Student Dashboard</h1>
+          <h1 className='text-[36px] leading-[40px] font-bold font-montserrat'>Member Dashboard</h1>
         </div>
         <div>
-          <h4>Enroll Classes</h4>
+          <div>
+            <h4>Enroll Classes</h4>
+          </div>
           <div className="inline-flex gap-8 p-8">
-            <div>
-              <img src="/src/assets/images/yogaimage.png" alt="yoga image" loading="lazy" />
-              <span>Yoga Class</span>
-            </div>
-            <div>
-              <img src="/src/assets/images/cardio.png" alt="cardio image" loading="lazy" />
-              <span>Cardio Class</span>
-            </div>
-            <div>
-              <img src="\src\assets\images\strenth.png" alt="Stremthing image" loading="lazy" />
-              <span>Strnthing Class</span>
-            </div>
+
+              <div>
+                <div className="w-[165px] h-[166px]">
+                    <img src="/src/assets/images/yogaimage.png" alt="yoga image" loading="lazy" />
+                </div>
+                <div>
+                    <p>Yoga for Beginners</p>
+                    <span>Instructor: Lily Harpa</span>
+                </div>
+              </div>
+
+              <div>
+                  <div className="w-[165px] h-[166px]">
+                    <img src="\src\assets\images\strenth.png" alt="Stremthing image" loading="lazy" />
+                  </div>
+                  <div>
+                     <p>Strenth Training</p>
+                     <span>Instructor: Owen Bennett</span>
+                  </div>
+              </div>
+
+              <div>
+                  <div className="w-[165px] h-[166px]">
+                    <img src="/src/assets/images/cardio.png" alt="cardio image" loading="lazy"  />
+                  </div>
+                  <div>
+                    <div>
+                        <p>Cardio Blast</p>
+                        <span>Instructor: Avo Faster</span>
+                    </div>
+                  </div>
+              </div>
+
           </div>
         </div>
+        <div>
+          <div>
+            <h4>Next Upcoming Class</h4>
+          </div>
+
+          <div>
+              <div>
+                <div></div>
+                <div></div>
+              </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <h4>Fitness Progress</h4>
+          </div>
+        <div>
+
+         </div>
+          <div>
+            <button>Payment</button>
+            <button>Class Registration</button>
+          </div>
+        </div>
+
       </main>
+
     </div>
   );
 }
