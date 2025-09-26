e1306f51dd9f7e66aa2a902f79918284c1558efd
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-26 07:25:04 -0700

    fix route error

--- src/Components/landingPage.jsx
+++ src/Components/landingPage.jsx
@@ -39,7 +39,7 @@ export default function LandingPage() {
                 <button
                   className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#223649] text-white text-sm font-Poppins font-bold leading-normal tracking-[0.015em] w-full"
                 >
-                  <NavLink to="/student" className="w-full h-full flex items-center justify-center">
+                  <NavLink to="/member" className="w-full h-full flex items-center justify-center">
                     <span className="truncate">Member Login</span>
                   </NavLink>
                 </button>
