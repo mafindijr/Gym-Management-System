ca0d6486213b7a58886d1c216320afeaab7f61a2
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-28 12:13:44 -0700

    feat: added form logic to handle form inputs

--- src/Components/Auth/signUp.jsx
+++ src/Components/Auth/signUp.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function SignUpPage() {
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [form, setForm] = useState({
+        fullName: "",
+        email: "",
+        createPassword: "",
+        confirmPassword: ""
+      });
+    
+      const handleChange = e => {
+        const { name, value } = e.target;
+        setForm(prev => ({ ...prev, [name]: value }));
+      };
+    
+      const handleSubmit = e => {
+        e.preventDefault();
+        // Here you would handle updating payment/card details
+        alert("Payment details updated!");
+      };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
             <div className="w-full bg-[#223649] p-6 rounded-lg shadow flex flex-col gap-4">
                 <div>
                     <h2 className='text-[28px] leading-[35px] font-bold text-center'>Welcome! Sign Up</h2>
@@ -11,6 +32,8 @@ export default function SignUpPage() {
                     <input 
                     type="text"
                     name='fullName'
+                    value={form.fullName}
+                    onChange={handleChange}
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
                     placeholder='Full Name'
                     required
@@ -18,8 +41,10 @@ export default function SignUpPage() {
                 </div>
                 <div>
                     <input 
-                    type="Email"
-                    name='Email'
+                    type="email"
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
                     placeholder='Enter Email'
                     required
@@ -29,6 +54,8 @@ export default function SignUpPage() {
                     <input 
                     type="password"
                     name='createPassword'
+                    value={form.createPassword}
+                    onChange={handleChange}
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
                     placeholder='Create Password'
                     required
@@ -37,14 +64,19 @@ export default function SignUpPage() {
                 <div>
                     <input 
                     type="password"
-                    name='confirmEmail'
+                    name='confirmPassword'
+                    value={form.confirmPassword}
+                    onChange={handleChange}
                     className="p-2 my-1 w-full outline-none border-[#334d66] border-2 bg-[#223649] rounded-md text-[16px] text-[#91adc9]"
                     placeholder='Confirm Password'
                     required
                     />
                 </div>
                 <div className='flex flex-col items-center'>
-                    <button className='bg-btnprimary w-full pl-4 pr-4 rounded-[24px] text-[14px] font-bold font-poppins text-center cursor-pointer h-[40px]'>Sign Up</button>
+                    <button 
+                     type='submit'
+                     className='bg-btnprimary w-full pl-4 pr-4 rounded-[24px] text-[14px] font-bold font-poppins text-center cursor-pointer h-[40px]'
+                     >Sign Up</button>
                     <span className='text-[14px] leading-[21px] text-[#91adc9]'>Already have an account? Log In</span>
                 </div>
                 
