c3c3f099e0ed7ded777022f005836f6b7a9150e8
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-09-28 12:13:13 -0700

    remove on wanted logic from the landing page

--- src/Components/landingPage.jsx
+++ src/Components/landingPage.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
 import Modal from "./modal"
 import LoginPage from './Auth/login'
 
@@ -54,10 +53,8 @@ export default function LandingPage() {
                 <button
                   className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-[#223649] text-white text-sm font-Poppins font-bold leading-normal tracking-[0.015em] w-full"
                   onClick={() => setOpenModal(true)}
-                >
-                  {/* <NavLink to="/admin"> */}
+                >  
                   <span className="truncate">Admin Login</span>
-                  {/* </NavLink> */}
                 </button>
               </div>
             </div>
